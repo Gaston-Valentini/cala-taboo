399baa5ce214654bfd0093cd2bad91ff4d929fe3
Gaston-Valentini <gastonvalentiniruiz@gmail.com>
2024-06-03 12:04:16 +0200

    Add: Navbar component added.

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,76 @@
+import style from "./Navbar.module.css";
+import { useTranslation } from "react-i18next";
+import logo from "../../assets/images/logo.png";
+import { IoMdClose } from "react-icons/io";
+import flagSpain from "../../assets/images/flag-spain.png";
+import flagEngland from "../../assets/images/flag-england.png";
+import { FaBars } from "react-icons/fa6";
+import { useState } from "react";
+
+export default function Navbar() {
+    const [t, i18n] = useTranslation("global");
+    const [language, setLanguage] = useState("es");
+    const [menu, setMenu] = useState(false);
+
+    const onLanguage = (e) => {
+        const { value } = e.target;
+        i18n.changeLanguage(value);
+        setLanguage(value);
+    };
+
+    const onMenu = () => {
+        setMenu(!menu);
+    };
+
+    return (
+        <nav className={style.container}>
+            <div className={style.logo}>
+                <img src={logo} />
+            </div>
+            <div className={menu ? `${style.overlay} ${style.overlayActive}` : style.overlay}>
+                <ul className={menu ? `${style.links} ${style.linksActive}` : style.links}>
+                    <IoMdClose className={style.linksClose} onClick={onMenu} />
+                    <li className={style.linksLink}>
+                        <a href="#" onClick={onMenu}>
+                            {t("navbar.linkOne")}
+                        </a>
+                    </li>
+                    <li className={style.linksLink}>
+                        <a href="#" onClick={onMenu}>
+                            {t("navbar.linkTwo")}
+                        </a>
+                    </li>
+                    <li className={style.linksLink}>
+                        <a href="#" onClick={onMenu}>
+                            {t("navbar.linkThree")}
+                        </a>
+                    </li>
+                    <li className={style.linksLink}>
+                        <a href="#" onClick={onMenu}>
+                            {t("navbar.linkFour")}
+                        </a>
+                    </li>
+                    <li className={style.linksLink}>
+                        <a href="#" onClick={onMenu}>
+                            {t("navbar.linkFive")}
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div className={style.buttons}>
+                <div className={style.buttonsLanguage}>
+                    <div className={style.buttonsLanguageFlag}>
+                        {language === "es" && <img src={flagSpain} />}
+                        {language === "en" && <img src={flagEngland} />}
+                    </div>
+                    <select className={style.buttonsLanguageSelector} onChange={(e) => onLanguage(e)}>
+                        <option hidden>{t("navbar.language")}</option>
+                        {language !== "es" && <option value="es">{t("navbar.spanish")}</option>}
+                        {language !== "en" && <option value="en">{t("navbar.english")}</option>}
+                    </select>
+                </div>
+                <FaBars className={style.buttonsBars} onClick={onMenu} />
+            </div>
+        </nav>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+import esLocale from "./locales/es/global.json";
+import enLocale from "./locales/en/global.json";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+i18next.init({
+    interpolation: { escapeValue: false },
+    lng: "es",
+    resources: {
+        es: {
+            global: esLocale,
+        },
+        en: {
+            global: enLocale,
+        },
+    },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <I18nextProvider i18n={i18next}>
+            <App />
+        </I18nextProvider>
+    </React.StrictMode>
+);
